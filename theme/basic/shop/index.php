2c0135ba515284d4551c1db89f3062ff27da1a15
Temirov Ilkom <ilhomtemirov9601@gmail.com>
2021-12-24 23:58:30 +0900

    feat: prepare the header part

--- theme/basic/shop/index.php
+++ theme/basic/shop/index.php
@@ -8,7 +8,9 @@ if (G5_IS_MOBILE) {
 
 if(! defined('_INDEX_')) define('_INDEX_', TRUE);
 
-include_once(G5_THEME_SHOP_PATH.'/shop.head.php');
+//include_once(G5_THEME_SHOP_PATH.'/shop.head.php');
+include_once(G5_THEME_SHOP_PATH.'/shop.head_r.php');
+
 ?>
 
 <!-- 메인이미지 시작 { -->
---
+++ theme/basic/shop/shop.head_r.php
@@ -0,0 +1,53 @@
+<?php
+if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
+
+$q = isset($_GET['q']) ? clean_xss_tags($_GET['q'], 1, 1) : '';
+
+if(G5_IS_MOBILE) {
+    include_once(G5_THEME_MSHOP_PATH.'/shop.head.php');
+    return;
+}
+
+include_once(G5_THEME_PATH.'/head.sub.php');
+include_once(G5_LIB_PATH.'/outlogin.lib.php');
+include_once(G5_LIB_PATH.'/poll.lib.php');
+include_once(G5_LIB_PATH.'/visit.lib.php');
+include_once(G5_LIB_PATH.'/connect.lib.php');
+include_once(G5_LIB_PATH.'/popular.lib.php');
+include_once(G5_LIB_PATH.'/latest.lib.php');
+
+add_javascript('<script src="'.G5_JS_URL.'/owlcarousel/owl.carousel.min.js"></script>', 10);
+add_stylesheet('<link rel="stylesheet" href="'.G5_JS_URL.'/owlcarousel/owl.carousel.css">', 0);
+?>
+
+<!-- 전체 콘텐츠 시작 { -->
+<div id="wrapper" class="<?php echo implode(' ', $wrapper_class); ?>">
+    <!-- #container 시작 { -->
+    <div id="container">
+
+        <div>Hello Word</div>
+
+        <?php if(defined('_INDEX_')) { ?>
+        <?php } // end if ?>
+        <?php
+            $content_class = array('shop-content');
+            if( isset($it_id) && isset($it) && isset($it['it_id']) && $it_id === $it['it_id']){
+                $content_class[] = 'is_item';
+            }
+            if( defined('IS_SHOP_SEARCH') && IS_SHOP_SEARCH ){
+                $content_class[] = 'is_search';
+            }
+            if( defined('_INDEX_') && _INDEX_ ){
+                $content_class[] = 'is_index';
+            }
+        ?>
+        <!-- .shop-content 시작 { -->
+        <div class="<?php echo implode(' ', $content_class); ?>">
+            <?php if ((!$bo_table || $w == 's' ) && !defined('_INDEX_')) { ?><div id="wrapper_title"><?php echo $g5['title'] ?></div><?php } ?>
+            <!-- 글자크기 조정 display:none 되어 있음 시작 { -->
+            <div id="text_size">
+                <button class="no_text_resize" onclick="font_resize('container', 'decrease');">작게</button>
+                <button class="no_text_resize" onclick="font_default('container');">기본</button>
+                <button class="no_text_resize" onclick="font_resize('container', 'increase');">크게</button>
+            </div>
+            <!-- } 글자크기 조정 display:none 되어 있음 끝 -->
